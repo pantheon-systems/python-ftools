f138d68e09d6d75fcfdfcc23bc96cd2252c8ec8c
david415 <dstainton415@gmail.com>
2010-09-05 08:26:57 -0700

    more descriptive exceptions and got rid of tabs

--- fincore.c
+++ fincore.c
@@ -5,60 +5,59 @@
 
 // fincore.fincore(fileno)
 static PyObject *fincore_fincore(PyObject *self, PyObject *args) {
-	PyObject *ret;
-	int fd;
-	void *file_mmap;
-	unsigned char *mincore_vec;
-	struct stat file_stat;
-	ssize_t page_size = getpagesize();
-	ssize_t vec_size;
+    PyObject *ret;
+    int fd;
+    void *file_mmap;
+    unsigned char *mincore_vec;
+    struct stat file_stat;
+    ssize_t page_size = getpagesize();
+    ssize_t vec_size;
 
-	if(!PyArg_ParseTuple(args, "i", &fd)) {
-		return NULL;
-	}
-
-	if(fstat(fd, &file_stat) < 0) {
-		PyErr_SetFromErrno(PyExc_IOError);
-		return NULL;
-	}
-
-    // cannot mmap a zero size file
-    if ( file_stat.st_size == 0 ) {
-        PyErr_SetFromErrno(PyExc_IOError);
+    if(!PyArg_ParseTuple(args, "i", &fd)) {
         return NULL;
     }
 
-	file_mmap = mmap((void *)0, file_stat.st_size, PROT_NONE, MAP_SHARED, fd, 0);
+    if(fstat(fd, &file_stat) < 0) {
+        PyErr_SetString(PyExc_IOError, "Could not fstat file");
+        return NULL;
+    }
 
-	if(file_mmap == MAP_FAILED) {
-		PyErr_SetFromErrno(PyExc_IOError);
-		return NULL;
-	}
+    if ( file_stat.st_size == 0 ) {
+        PyErr_SetString(PyExc_IOError, "Cannot mmap zero size file");
+        return NULL;
+    }
 
-	vec_size = (file_stat.st_size + page_size - 1) / page_size;
-	mincore_vec = calloc(1, vec_size);
+    file_mmap = mmap((void *)0, file_stat.st_size, PROT_NONE, MAP_SHARED, fd, 0);
 
-	if(mincore_vec == NULL) {
-		PyErr_SetFromErrno(PyExc_IOError);
-		return NULL;
-	}
+    if(file_mmap == MAP_FAILED) {
+        PyErr_SetString(PyExc_IOError, "Could not mmap file");
+        return NULL;
+    }
 
-	if(mincore(file_mmap, file_stat.st_size, mincore_vec) != 0) {
-		PyErr_SetFromErrno(PyExc_OSError);
-		return NULL;
-	}
+    vec_size = (file_stat.st_size + page_size - 1) / page_size;
+    mincore_vec = calloc(1, vec_size);
 
-	ret = Py_BuildValue("s#", mincore_vec, vec_size);
-	free(mincore_vec);
-	munmap(file_mmap, file_stat.st_size);
-	return ret;
+    if(mincore_vec == NULL) {
+        return PyErr_NoMemory();
+    }
+
+    if(mincore(file_mmap, file_stat.st_size, mincore_vec) != 0) {
+        PyErr_SetFromErrno(PyExc_OSError);
+        PyErr_SetString(PyExc_OSError, "Could not call mincore for file");
+        return NULL;
+    }
+
+    ret = Py_BuildValue("s#", mincore_vec, vec_size);
+    free(mincore_vec);
+    munmap(file_mmap, file_stat.st_size);
+    return ret;
 }
 
 static PyMethodDef FincoreMethods[] = {
-	{"fincore", fincore_fincore, METH_VARARGS, "Return the mincore structure for the given file."},
-	{NULL, NULL, 0, NULL}
+    {"fincore", fincore_fincore, METH_VARARGS, "Return the mincore structure for the given file."},
+    {NULL, NULL, 0, NULL}
 };
 
 PyMODINIT_FUNC initfincore(void) {
-	(void)Py_InitModule("fincore", FincoreMethods);
+    (void)Py_InitModule("fincore", FincoreMethods);
 }
