2164eb2aa2ef86b5bcadf23c313c91d8c8021f74
root <root@util1.(none)>
2010-11-19 20:11:10 +0000

    new setup.py so we can create debian packages

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from distutils.core import setup, Extension
 
 setup(name='python-ftools',
-      version='0.1',
-      ext_modules=[
+    version='0.1',
+    author='David Stainton',
+    author_email='dstainton415@gmail.com',
+    ext_modules=[
           Extension('ftools', ['ftools.c']),
-      ])
+      ],
+      scripts=['scripts/python_fincore','scripts/python_fadvise','scripts/pages_by_time','scripts/report_filechanges']
+)
+
