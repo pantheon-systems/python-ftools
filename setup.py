4a4cc306edb387ca8e7b7f21a97bcf58e0d7d644
joe miller <joeym@joeym.net>
2014-01-07 00:03:19 +0000

    make scripts more resilient to special files.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 
 setup(name='python-ftools',
-    version='0.1',
+    version='0.1.pantheon1',
     author='David Stainton',
     author_email='dstainton415@gmail.com',
     ext_modules=[
